505f23e532c545f2bf43d92e420b1e5bfda0b1fa
denny310 <45156106+denny310@users.noreply.github.com>
2018-11-19 11:57:31 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,7 +1,31 @@
 <?php
-  $json_str=file_get_contents('php://input');
-  $json_obj=json_decode($json_str);
+  $json_str = file_get_contents('php://input'); //接收request的body
+  $json_obj = json_decode($json_str); //轉成json格式
   
-  $myfile=fopen("log.txt","w")or die("unable to open file");
-  fwrite($myfile,"\xEF\xBB\xBF".$json_str);
- ?>
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+  
+  $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
+  $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+  
+  $response = array (
+    "to" => $sender_userid,
+    "messages" => array (
+      array (
+        "type" => "text",
+        "text" => "Hello. You say". $sender_txt
+      )
+    )
+  );
+  
+ fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+  $header[] = "Content-Type: application/json";
+  $header[] = "Authorization:hSdnk0b9MspwhvlUAKkQRicIiwsucnKDeST8whjhFsgHBIn5PGnduqDlli5F18WaSiz9hecM3ehQZN+nv1d9wNmF3I8UYtQ8SJ2c1I178JMWQPexMJNJY3CHD2Z2AObysM901SLQNkxIYx2V4Vbg0AdB04t89/1O/w1cDnyilFU=";
+  $ch = curl_init("https://api.line.me/v2/bot/message/push");
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
+  $result = curl_exec($ch);
+  curl_close($ch);
+?>
