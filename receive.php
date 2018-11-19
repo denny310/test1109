2166b43e206b033641d2fa84d00d0b45435786f2
denny310 <45156106+denny310@users.noreply.github.com>
2018-11-19 15:23:58 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -67,7 +67,7 @@
   
   fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   $header[] = "Content-Type: application/json";
-  $header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+  $header[] = "Authorization: Bearer kqqMhbUajwv72s1+TGeFOeFpXt7Ax4M2U24CqJDXI04sIrnkPpox758Gtg2XJaEZSiz9hecM3ehQZN+nv1d9wNmF3I8UYtQ8SJ2c1I178JOPjcw9Of25TgLPstQqRYfBGTxTH4NQgeWl6HES8AIMgAdB04t89/1O/w1cDnyilFU=";
   $ch = curl_init("https://api.line.me/v2/bot/message/reply");
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
